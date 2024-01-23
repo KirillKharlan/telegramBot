c2e2c821340b838699dc2f1b21509424520ed1dc
Illya <121565465+IllyaEpik@users.noreply.github.com>
2024-01-23 17:44:46 +0200

    TelegramBot

--- main.py
+++ main.py
@@ -9,21 +9,47 @@ import modules.image as m_image
 import modules.keyboard as m_keyboard
 @m_data.dp.message(filters.CommandStart())
 async def start(message:aiogram.types.Message):
+    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS {message.from_user.username} (INTEGER PRIMARY KEY,id)")
+    m_sqlite.add_column(name_column="burger",type_column="INTEGER",name_table=message.from_user.username)
+    m_sqlite.add_column(name_column="hotDog",type_column="INTEGER",name_table=message.from_user.username)
     await message.answer(text="Hello",reply_markup=m_keyboard.create_keyboard([["hot-dog","burger"]]))
     # #await m_image.image("burger.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
 @m_data.dp.message()
 async def continue_1(message:aiogram.types.Message):
-    print(message.text)
+    print(message) 
+    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS {message.from_user.username} (INTEGER PRIMARY KEY,id)")
+    m_sqlite.add_column(name_column="burger",type_column="INTEGER",name_table=message.from_user.username)
+    m_sqlite.add_column(name_column="hotDog",type_column="INTEGER",name_table=message.from_user.username)
     if message.text=="burger":
         await m_image.image("burger.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
     elif message.text=="hot-dog":
         await m_image.image("hotDog.jpg",message,reply_markup=m_keyboard.create_inline_keyboard())
 @m_data.dp.callback_query()
 async def call_back(callback:aiogram.types.callback_query.CallbackQuery):
-    print(callback.data)
+    m_sqlite.cursor.execute(f"CREATE TABLE IF NOT EXISTS {callback.message.chat.username} (INTEGER PRIMARY KEY,id)")
+    m_sqlite.add_column(name_column="burger",type_column="INTEGER",name_table=callback.message.chat.username)
+    m_sqlite.add_column(name_column="hotDog",type_column="INTEGER",name_table=callback.message.chat.username)
+    m_sqlite.add_column(name_column="ok",type_column="INTEGER",name_table=callback.message.chat.username)
+    print(callback.message.photo)
+    # 8266 - burger  AgACAgIAAxkDAAIBBGWuuJ0oMnJYwIpf95NkRpK35TbWAALt0jEbntNRSQHvDJ-4XD86AQADAgADcwADNAQ
+    # 5332 - hot-dog AgACAgIAAxkDAAIBAmWuuIXU9cUzyesDz2PqgdMbKnp8AAL30jEbntNRSWfv-yIKPsJNAQADAgADcwADNAQ
+    inline_keyboard =callback.message.reply_markup
+    if callback.data== "Accept":
+        print(callback.message.photo[0].file_size)
+        if callback.message.photo[0].file_size==1320:
+            product="burger"
+        else:
+            product="hotDog"
+        count=callback.message.reply_markup.inline_keyboard[0][0].callback_data.split()[1]
+        print(product,count)
+        m_sqlite.set_value(columns=(product,"ok"),values=(count,count),name_table=callback.message.chat.username)
+        m_sqlite.data.commit()
+        inline_keyboard.inline_keyboard[0][0].text="Buy"
+        inline_keyboard.inline_keyboard[0][0].callback_data="Buy 0"
+        del inline_keyboard.inline_keyboard[1]
+        await callback.message.edit_reply_markup(reply_markup=inline_keyboard,inline_message_id=callback.inline_message_id)
     if 'Buy' in callback.data:
-        inline_keyboard =callback.message.reply_markup
-        count = '0'
+        count = '1'
         if 'Buy' != callback.data:
             count = callback.data.split(' ')
             print(count)
@@ -41,9 +67,11 @@ async def call_back(callback:aiogram.types.callback_query.CallbackQuery):
         inline_keyboard =callback.message.reply_markup
         count = '0'
         if 'Buy' != callback.message.reply_markup.inline_keyboard[0][0].callback_data:
-            count = callback.message.reply_markup.inline_keyboard[0][0].callback_data.split(' ')
+            count1 = callback.message.reply_markup.inline_keyboard[0][0].callback_data.split(' ')
             print(count)
-            count = str(int(count[-1])-1)
+            if int(count1[-1])>0:
+                count = str(int(count1[-1])-1)
+
             print(type(count))
             
         print('0'==count)
@@ -51,7 +79,11 @@ async def call_back(callback:aiogram.types.callback_query.CallbackQuery):
         print()
         inline_keyboard.inline_keyboard[0][0].callback_data = f'Buy {count}'
         inline_keyboard.inline_keyboard[0][0].text = f'Buy {count}'
-        await callback.message.edit_reply_markup(reply_markup=inline_keyboard,inline_message_id=callback.inline_message_id)
+        
+        try:
+            await callback.message.edit_reply_markup(reply_markup=inline_keyboard,inline_message_id=callback.inline_message_id)
+        except:
+            print("Telegeram error")
     # if len(inline_keyboard)== 1:
 
     #     # inline_keyboard.append([aiogram.types.InlineKeyboardButton(text="accept",callback_data="accept")])
